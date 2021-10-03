32e726d5d860febbbba80a11ac410e428054d370
gingledoof <62526960+gingledoof@users.noreply.github.com>
2021-10-02 22:12:28 -0400

    Update

--- mark_master.py
+++ mark_master.py
@@ -6,14 +6,6 @@ getSearch('music')
 
 luke_token = 'NzIyMjU5MDE0NTkwOTIyNzcy.XugeMw.VAzM8baYc-M4cKl0y85UB1Dzdps'
 
-TOKENS = (
-          'NzIzMzY0MjM0ODAwNTI5NDM4.Xuwjgg.9LIOvzKVvdg51T8mbPIvylMvwEQ',
-          'NzIzMzg5NTQ3MzcwNzc0NTg5.Xuw7Eg.DEEP5t6mQ79U8TRdvwUipfjc1Pw',
-          'NzIzMzg5OTA0OTg5NDU0NDQ5.Xuw7aA.vuo1KunaRuVLpEpkPFqJUwOoSGY',
-          'NzIzMzkwMjg0MDU4MTk4MTM4.Xuw7xg.W3i3YXvE6n0s55Eaob-DWfdI5Qk',
-          'NzIzNDAzNTI3NDc2NDc4MDEz.XuxUgg.fseE1QuUATEjKvm73eNh-pjJCfc',
-          'NzIzMzYyMDk5NDg4ODgyNjk4.XuwhpQ.SmwN51X7lMIJSrCOIKiHsQo5d-w',)
-
 
 games = [
     "Xonotic",
---
+++ tokens.py
@@ -0,0 +1,7 @@
+TOKENS = (
+          'NzIzMzY0MjM0ODAwNTI5NDM4.Xuwjgg.9LIOvzKVvdg51T8mbPIvylMvwEQ',
+          'NzIzMzg5NTQ3MzcwNzc0NTg5.Xuw7Eg.DEEP5t6mQ79U8TRdvwUipfjc1Pw',
+          'NzIzMzg5OTA0OTg5NDU0NDQ5.Xuw7aA.vuo1KunaRuVLpEpkPFqJUwOoSGY',
+          'NzIzMzkwMjg0MDU4MTk4MTM4.Xuw7xg.W3i3YXvE6n0s55Eaob-DWfdI5Qk',
+          'NzIzNDAzNTI3NDc2NDc4MDEz.XuxUgg.fseE1QuUATEjKvm73eNh-pjJCfc',
+          'NzIzMzYyMDk5NDg4ODgyNjk4.XuwhpQ.SmwN51X7lMIJSrCOIKiHsQo5d-w',)
